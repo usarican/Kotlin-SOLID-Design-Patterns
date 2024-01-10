21f5efcbf60ebd02b1b348cc76f9bdae94455dbb
ibrahimutkusarican <utku.sarican@rapsodo.com>
2024-01-10 13:18:10 +0300

    Example of Single Responsibility Principle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,26 @@
+import solid.SingleReponsibilityPrincipleExample.*
+
 fun main() {
-    println("Utku")
+    val userStore = UserStore()
+    val userStoreCorrect = UserStoreCorrect()
+    val userController = UserController(userStore)
+    val userControllerCorrect = UserControllerCorrect(userStoreCorrect)
+
+    val validUserData = User(10,"Utku")
+    val validUserData2 = User(35,"Aslı")
+    val invalidUserData = User(null,null)
+    val invalidUserData2 = User(10,"")
+
+    userController.createUser(validUserData)
+    userController.createUser(validUserData2)
+    userController.createUser(invalidUserData)
+    userController.createUser(invalidUserData)
+
+    userControllerCorrect.createUser(validUserData)
+    userControllerCorrect.createUser(validUserData2)
+    userControllerCorrect.createUser(invalidUserData2)
+    userControllerCorrect.createUser(invalidUserData2)
+
+    println(userStore.userList)
+    println(userStoreCorrect.getUserList())
 }
--- src/main/kotlin/solid/SingleReponsibilityPrincipleExample/CorrectUsage.kt
+++ src/main/kotlin/solid/SingleReponsibilityPrincipleExample/CorrectUsage.kt
@@ -1,4 +1,27 @@
 package solid.SingleReponsibilityPrincipleExample
 
-class CorrectUsage {
+class UserControllerCorrect(
+    private val userStore : UserStoreCorrect
+) {
+    private val userValidator = UserValidator()
+
+    fun createUser(newUser : User) {
+        if(userValidator.validateUser(newUser)){
+            userStore.addUserToList(newUser)
+        }
+    }
+}
+
+
+class UserStoreCorrect {
+    private val userList = mutableListOf<User>()
+    fun getUserList() = userList
+    fun addUserToList(newUser : User){
+        userList.add(newUser)
+    }
+}
+
+class UserValidator(){
+    fun validateUser(newUser: User) =
+        newUser.userId != null && newUser.userName != null && newUser.userName.isNotEmpty()
 }
