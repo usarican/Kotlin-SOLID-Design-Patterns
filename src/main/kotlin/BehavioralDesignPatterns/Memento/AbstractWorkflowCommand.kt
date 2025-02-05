326e6cf40f7d3a56a9b170e12630832a5efe1d5d
ibrahimutkusarican <iu.sarican@gmail.com>
2025-02-05 14:09:08 +0300

    Add java class.

--- src/main/kotlin/BehavioralDesignPatterns/Memento/AbstractWorkflowCommand.kt
+++ src/main/kotlin/BehavioralDesignPatterns/Memento/AbstractWorkflowCommand.kt
@@ -1,13 +1,13 @@
 package BehavioralDesignPatterns.Memento
 
 abstract class AbstractWorkflowCommand(
-    protected var memento: WorkflowDesigner.Memento? = null,
+    protected var memento: WorkflowDesigner.Memento = WorkflowDesigner.Memento(),
     protected open val designer: WorkflowDesigner
 ) : WorkflowCommand {
 
     override fun undo() {
         println("Undo Memento ${memento?.steps}")
-        memento?.let { designer.setMemento(it) }
+        memento.let { designer.setMemento(it) }
 
     }
 
--- src/main/kotlin/BehavioralDesignPatterns/Memento/AddStepCommand.kt
+++ src/main/kotlin/BehavioralDesignPatterns/Memento/AddStepCommand.kt
@@ -4,9 +4,13 @@ class AddStepCommand(
     override val designer: WorkflowDesigner, private val step: String,
 ) : AbstractWorkflowCommand(designer = designer) {
     override fun execute() {
-        super.memento = designer.getMemento()
-        println("Add Step Memento ${this.memento?.steps}")
+        this.memento = designer.getMemento()
+        println("Add Step Memento ${this.memento.steps}")
         designer.addStep(step)
+    }
+
+    override fun undo() {
+        designer.setMemento(this.memento)
     }
 
 }
